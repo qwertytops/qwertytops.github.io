13c0009bd1b2d0c7eb51ec3cfaa99c8e580218ad
LiamvdM <lvandermoezel@gmail.com>
2024-01-25 15:07:53 +0800

    changed stuff

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 function Continue(button) {
     document.getElementById("iii").innerHTML = ""
-    document.getElementById("hhh").innerHTML = `Dearest Janet,<br>
-    You've told me once and implied many a time that you don't want a close relationship with me or any guy right now, and
-    to be perfectly honest I don't know if I'm ready for you either.<br>
-    However, there is no one in the world as lovely as you, and it would be a crime to not at least give this a shot 
-    regardless of our age difference which was the lamest excuse I have ever heard by the way &#128522.
-    I am madly in love with you and have been for quite a while, 
-    and nothing would make me happier than to take you out and treat you like the Queen you are even just once. 
-    I am aware that this is possibly the least romantic way to ask someone out but here we are. 
-    Should you accept this offer, I will expect very little time from you because you're busy, I'm busy, 
-    and I will go broke within a month if I do. I haven't actually asked anything yet though so here's my question:<br>
-    Will you go out with me sometime and let me show you how much I love you? 
-    I want to talk to you in real life away from distractions and other people. 
-    All of our most fun and interesting conversations have been on Messenger which is a dismal state of affairs that I want to change.<br>
-    Respond in whatever manner you wish.<br><br>
-    From your loving friend,<br>
-    Liam &#128149<br><br>`
+    // document.getElementById("hhh").innerHTML = `Dearest Janet,<br>
+    // You've told me once and implied many a time that you don't want a close relationship with me or any guy right now, and
+    // to be perfectly honest I don't know if I'm ready for you either.<br>
+    // However, there is no one in the world as lovely as you, and it would be a crime to not at least give this a shot 
+    // regardless of our age difference which was the lamest excuse I have ever heard by the way &#128522.
+    // I am madly in love with you and have been for quite a while, 
+    // and nothing would make me happier than to take you out and treat you like the Queen you are even just once. 
+    // I am aware that this is possibly the least romantic way to ask someone out but here we are. 
+    // Should you accept this offer, I will expect very little time from you because you're busy, I'm busy, 
+    // and I will go broke within a month if I do. I haven't actually asked anything yet though so here's my question:<br>
+    // Will you go out with me sometime and let me show you how much I love you? 
+    // I want to talk to you in real life away from distractions and other people. 
+    // All of our most fun and interesting conversations have been on Messenger which is a dismal state of affairs that I want to change.<br>
+    // Respond in whatever manner you wish.<br><br>
+    // From your loving friend,<br>
+    // Liam &#128149<br><br>`
+    document.getElementById("hhh").innerHTML = "This is where the message was"
 
-    document.getElementById("jjj").innerHTML = `--DISCLAIMER FOR LEGAL REASONS--<br>
-    Although I'm not 100% sure what caused your 'bad experiences' with guys,
-    the two most likely reasons I can think of are not major issues in this case (I hope).<br>
-    Losing a friend - If you don't feel the same way about me I am perfectly happy to stay friends like we have been if you are<br>
-    Causing me heartbreak - Like I said, you have been quite clear on this. I have taken this risk and I would take it again<br>
-    If this has made me a 'bad experience' I could not be more sorry and I will do my best to make it up to you.`
+    // document.getElementById("jjj").innerHTML = `--DISCLAIMER FOR LEGAL REASONS--<br>
+    // Although I'm not 100% sure what caused your 'bad experiences' with guys,
+    // the two most likely reasons I can think of are not major issues in this case (I hope).<br>
+    // Losing a friend - If you don't feel the same way about me I am perfectly happy to stay friends like we have been if you are<br>
+    // Causing me heartbreak - Like I said, you have been quite clear on this. I have taken this risk and I would take it again<br>
+    // If this has made me a 'bad experience' I could not be more sorry and I will do my best to make it up to you.`
 }
